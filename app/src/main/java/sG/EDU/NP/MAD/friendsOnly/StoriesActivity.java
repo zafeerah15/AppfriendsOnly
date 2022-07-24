49d881802e9878f771d5c5dc4b11f712e9eb82db
Zafeerah15 <zafeerah15@gmail.com>
2022-07-24 13:37:56 +0800

    z

--- app/src/main/java/sG/EDU/NP/MAD/friendsOnly/StoriesActivity.java
+++ app/src/main/java/sG/EDU/NP/MAD/friendsOnly/StoriesActivity.java
@@ -10,161 +10,62 @@ import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import jp.shts.android.storiesprogressview.StoriesProgressView;
-public class StoriesActivity extends AppCompatActivity implements StoriesProgressView.StoriesListener {
+public class StoriesActivity {
+    private String imageurl;
+    private long timestart;
+    private long timeend;
+    private String storyid;
+    private String userid;
 
-    // on below line we are creating a int array
-    // in which we are storing all our image ids.
-    private final int[] resources = new int[]{
-            R.drawable.logo1,
-            R.drawable.logo2,
-            R.drawable.logo1,
-            R.drawable.logo2,
-            R.drawable.logo1,
-            R.drawable.logo2,
-    };
-
-    // on below line we are creating variable for
-    // our press time and time limit to display a story.
-    long pressTime = 0L;
-    long limit = 500L;
-
-    // on below line we are creating variables for
-    // our progress bar view and image view .
-    private StoriesProgressView storiesProgressView;
-    private ImageView image;
-
-    // on below line we are creating a counter
-    // for keeping count of our stories.
-    private int counter = 0;
-
-    // on below line we are creating a new method for adding touch listener
-    private View.OnTouchListener onTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View v, MotionEvent event) {
-            // inside on touch method we are
-            // getting action on below line.
-            switch (event.getAction()) {
-                case MotionEvent.ACTION_DOWN:
-
-                    // on action down when we press our screen
-                    // the story will pause for specific time.
-                    pressTime = System.currentTimeMillis();
-
-                    // on below line we are pausing our indicator.
-                    storiesProgressView.pause();
-                    return false;
-                case MotionEvent.ACTION_UP:
-
-                    // in action up case when user do not touches
-                    // screen this method will skip to next image.
-                    long now = System.currentTimeMillis();
-
-                    // on below line we are resuming our progress bar for status.
-                    storiesProgressView.resume();
-
-                    // on below line we are returning if the limit < now - presstime
-                    return limit < now - pressTime;
-            }
-            return false;
-        }
-    };
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        // inside in create method below line is use to make a full screen.
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_stories);
-
-        // on below line we are initializing our variables.
-        storiesProgressView = (StoriesProgressView) findViewById(R.id.stories);
-
-        // on below line we are setting the total count for our stories.
-        storiesProgressView.setStoriesCount(resources.length);
-
-        // on below line we are setting story duration for each story.
-        storiesProgressView.setStoryDuration(3000L);
-
-        // on below line we are calling a method for set
-        // on story listener and passing context to it.
-        storiesProgressView.setStoriesListener(this);
-
-        // below line is use to start stories progress bar.
-        storiesProgressView.startStories(counter);
-
-        // initializing our image view.
-        image = (ImageView) findViewById(R.id.image);
-
-        // on below line we are setting image to our image view.
-        image.setImageResource(resources[counter]);
-
-        // below is the view for going to the previous story.
-        // initializing our previous view.
-        View reverse = findViewById(R.id.reverse);
-
-        // adding on click listener for our reverse view.
-        reverse.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // inside on click we are
-                // reversing our progress view.
-                storiesProgressView.reverse();
-            }
-        });
-
-        // on below line we are calling a set on touch
-        // listener method to move towards previous image.
-        reverse.setOnTouchListener(onTouchListener);
-
-        // on below line we are initializing
-        // view to skip a specific story.
-        View skip = findViewById(R.id.skip);
-        skip.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // inside on click we are
-                // skipping the story progress view.
-                storiesProgressView.skip();
-            }
-        });
-        // on below line we are calling a set on touch
-        // listener method to move to next story.
-        skip.setOnTouchListener(onTouchListener);
+    public Story(String imageurl, long timestart, long timeend, String storyid, String userid){
+        this.imageurl = imageurl;
+        this.timestart = timestart;
+        this.timeend = timeend;
+        this.storyid = storyid;
+        this.userid = userid;
+    }
+    public Story(){
     }
 
-    @Override
-    public void onNext() {
-        // this method is called when we move
-        // to next progress view of story.
-        image.setImageResource(resources[++counter]);
+    public String getImageurl(){
+        return imageurl;
     }
 
-    @Override
-    public void onPrev() {
-
-        // this method id called when we move to previous story.
-        // on below line we are decreasing our counter
-        if ((counter - 1) < 0) return;
-
-        // on below line we are setting image to image view
-        image.setImageResource(resources[--counter]);
+    public void setImageurl(String imageurl)
+    {
+        this.imageurl = imageurl;
     }
 
-    @Override
-    public void onComplete() {
-        // when the stories are completed this method is called.
-        // in this method we are moving back to initial main activity.
-        Intent i = new Intent(StoriesActivity.this, MainActivity.class);
-        startActivity(i);
-        finish();
+    public long getTimestart(){
+        return timestart;
     }
 
-    @Override
-    protected void onDestroy() {
-        // in on destroy method we are destroying
-        // our stories progress view.
-        storiesProgressView.destroy();
-        super.onDestroy();
+    public void setTimestart(long timestart) {
+        this.timestart = timestart;
+    }
+
+    public long getTimeend(){
+        return timeend;
+    }
+
+    public void setTimeend(long timeend) {
+        this.timeend = timeend;
+    }
+
+    public String getStoryid(){
+        return storyid;
+    }
+
+    public void setStoryid(String storyid) {
+        this.storyid = storyid;
+    }
+
+    public String getUserid(){
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
     }
 }
+
